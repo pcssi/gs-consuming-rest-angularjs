90b76f739c7ab360d280645ada9cdd7f16714609
Pete Colapietro <pcssi@users.noreply.github.com>
2016-02-09 09:33:42 -0500

    Portential fix for provisioning Travis CI.

--- test/run.sh
+++ test/run.sh
@@ -1,12 +1,4 @@
-cd $(dirname $0)
-
-curl -s get.sdkman.io | bash
-sleep 10
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-echo "Y" | sdk install springboot 1.3.0.RELEASE
-sdk use springboot 1.3.0.RELEASE
-
-cd ../complete
+cd ./complete
 spring run app.groovy &
 sleep 20
 curl -s http://localhost:8080 > actual.html
