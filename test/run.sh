750192bfce2026a268a075f6b860c61443e972b9
Pete Colapietro <pcssi@users.noreply.github.com>
2016-02-09 10:09:35 -0500

    Brought sleep town down a bit.

--- test/run.sh
+++ test/run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 cd ./complete
 spring run app.groovy &
-sleep 20
+sleep 10
 curl -s http://localhost:8080 > actual.html
 killall "java"
 
 echo "Let's look at the actual results: `cat actual.html`"
 echo "And compare it to: `cat ../test/expected.html`"
 
-if diff -w ../test/expected.html actual.html; then
+if diff -u -w --strip-trailing-cr ../test/expected.html actual.html; then
   echo SUCCESS || exit 0
 else
   echo FAIL || exit 255
