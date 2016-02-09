434b2b0d8940f38846a99460ad9761f77e7228e3
Pete Colapietro <pcssi@users.noreply.github.com>
2016-02-09 10:20:05 -0500

    CI still returing failure on diff...

--- test/run.sh
+++ test/run.sh
@@ -8,10 +8,13 @@ killall "java"
 echo "Let's look at the actual results: `cat actual.html`"
 echo "And compare it to: `cat ../test/expected.html`"
 
-if diff -u -w --strip-trailing-cr ../test/expected.html actual.html; then
-  echo SUCCESS || RET=0
+if diff -u -w --strip-trailing-cr ../test/expected.html actual.html;
+then
+  echo SUCCESS
+  let RET=0
 else
-  echo FAIL || RET=255
+  echo FAIL
+  let RET=255
 fi
 rm -rf actual.html
 exit $RET
